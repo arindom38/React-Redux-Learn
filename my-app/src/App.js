4550fc32bdd9462adbb7a7bfdabfc43526bcfd7e
ujjal.kundu <arindomkundu18@gmail.com>
2021-06-16 13:33:56 +0600

    New componenst added

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import Users from "./Users";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>My First React App!</h1>
+        <p>Welcome to this cruel world</p>
+      </header>
+      <Users/>
+    </div>
+  );
+}
+
+export default App;
---
+++ my-app/src/Users.js
@@ -0,0 +1,12 @@
+
+function Users(){
+    return (
+        <div className='Users'>
+            <div>Name: Arindom</div>
+            <div>Age: 26</div>
+            <div>Job: Developer</div>
+        </div>
+    );
+}
+
+export default Users;
